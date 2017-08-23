fdec9c6f037fe890f73fcb9560af449b2e667c58
zxb <2507817568@qq.com>
2017-08-23 15:33:55 +0800

    解决jlogstash停止问题

--- mongodb/src/main/java/com/dtstack/jlogstash/outputs/MongoDB.java
+++ mongodb/src/main/java/com/dtstack/jlogstash/outputs/MongoDB.java
@@ -1,13 +1,19 @@
 package com.dtstack.jlogstash.outputs;
 
 import com.dtstack.jlogstash.annotation.Required;
+import com.dtstack.jlogstash.render.Formatter;
+import com.dtstack.jlogstash.utils.ThreadPoolUtil;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.GridFSUploadStream;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import com.mongodb.client.model.*;
 import org.bson.BsonDocument;
+import org.bson.BsonObjectId;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +26,16 @@ import java.util.concurrent.*;
 /**
  * @author zxb
  * @version 1.0.0
- *          2017年03月24日 10:34
+ * 2017年03月24日 10:34
  * @since Jdk1.6
  */
 public class MongoDB extends BaseOutput {
 
-    private static final Logger logger = LoggerFactory.getLogger(MongoDB.class);
+    public static final String RECEIVER_THREAD_NAME = "MongoDB-ReceiverThread";
+    public static final String BULK_THREAD_NAME = "MongoDB-BulkThread";
+    public static final int QUEUE_CAPACITY = 5000;
+    public static final int BULK_POOL_SIZE = 5;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoDB.class);
 
     @Required(required = true)
     private static String uri;
@@ -35,50 +45,57 @@ public class MongoDB extends BaseOutput {
 
     @Required(required = true)
     private static String collection;
+    /**
+     * 需要上传到GridFS的列名
+     */
+    private static Map<String, String> gridfsColumns;
+
+    /** GridFS文件块大小，单位：字节（byte） */
+    private static Integer chunkSize = 358400;
 
     /**
-     * 15秒提交一次
+     * 是否需要做GridFS处理
      */
-    private static Integer flush_interval = 15;
-
+    private static boolean needHandleGridFS;
     private static Integer size = 3000;
 
+    private GridFSUploadOptions options;
     /**
      * 操作类型，可选值insert,update,upsert
      */
     private String action = "insert";
-
     private MongoClient client;
-
     private MongoDatabase database;
-
+    private GridFSBucket gridFSBucket;
     private MongoCollection<Document> coll;
-
     private volatile BlockingQueue<WriteModel<Document>> writeModelQueue;
-
     private Action actionType;
-
-    private ExecutorService executor = Executors.newSingleThreadExecutor();
-
-    private BulkThread bulkThread;
-
-    private Future<?> future;
+    private ExecutorService receiverThreadExecutor = ThreadPoolUtil.newSingleThreadExecutor(QUEUE_CAPACITY, RECEIVER_THREAD_NAME);
+    private ExecutorService bulkThreadExecutor = ThreadPoolUtil.newFixedThreadPool(BULK_POOL_SIZE, QUEUE_CAPACITY, BULK_THREAD_NAME);
 
     public MongoDB(Map config) {
         super(config);
     }
 
     public void prepare() {
+        if (!needHandleGridFS && gridfsColumns != null && gridfsColumns.size() > 0) {
+            needHandleGridFS = true;
+        }
+
         client = new MongoClient(new MongoClientURI(uri));
         database = client.getDatabase(db_name);
         coll = database.getCollection(collection);
+
+        if (needHandleGridFS) {
+            gridFSBucket = GridFSBuckets.create(database);
+            options = new GridFSUploadOptions().chunkSizeBytes(chunkSize);
+        }
 
         writeModelQueue = new LinkedBlockingQueue<WriteModel<Document>>();
 
         initAction();
 
-        bulkThread = new BulkThread();
-        future = executor.submit(bulkThread);
+        receiverThreadExecutor.submit(new ReceiverThread());
     }
 
     private void initAction() {
@@ -89,35 +106,79 @@ public class MongoDB extends BaseOutput {
     }
 
     protected void emit(Map event) {
+        if (needHandleGridFS) {
+            handleGridFS(event);
+        }
         Document document = new Document(event);
-        if (actionType == Action.INSERT) {
-            writeModelQueue.offer(new InsertOneModel<Document>(document));
-        } else if (actionType == Action.UPDATE) {
-            Object id = document.get("_id");
-            BsonDocument bsonDocument = document.toBsonDocument(null, MongoClient.getDefaultCodecRegistry());
-            BsonDocument update = new BsonDocument("$set", bsonDocument);
-            UpdateOneModel<Document> updateOneModel = new UpdateOneModel<Document>(new Document("_id", id), update);
-            writeModelQueue.offer(updateOneModel);
-        } else if (actionType == Action.UPSERT) {
-            Object id = document.get("_id");
-            BsonDocument bsonDocument = document.toBsonDocument(null, MongoClient.getDefaultCodecRegistry());
-            BsonDocument update = new BsonDocument("$set", bsonDocument);
-            UpdateOneModel<Document> updateOneModel = new UpdateOneModel<Document>(new Document("_id", id), update, new UpdateOptions().upsert(true));
-            writeModelQueue.offer(updateOneModel);
+        try {
+            if (actionType == Action.INSERT) {
+                writeModelQueue.put(new InsertOneModel<Document>(document));
+            } else if (actionType == Action.UPDATE) {
+                Object id = document.get("_id");
+                BsonDocument bsonDocument = document.toBsonDocument(null, MongoClient.getDefaultCodecRegistry());
+                BsonDocument update = new BsonDocument("$set", bsonDocument);
+                UpdateOneModel<Document> updateOneModel = new UpdateOneModel<Document>(new Document("_id", id), update);
+                writeModelQueue.put(updateOneModel);
+            } else if (actionType == Action.UPSERT) {
+                Object id = document.get("_id");
+                BsonDocument bsonDocument = document.toBsonDocument(null, MongoClient.getDefaultCodecRegistry());
+                BsonDocument update = new BsonDocument("$set", bsonDocument);
+                UpdateOneModel<Document> updateOneModel = new UpdateOneModel<Document>(new Document("_id", id), update, new UpdateOptions().upsert(true));
+                writeModelQueue.put(updateOneModel);
+            }
+        } catch (InterruptedException e) {
+            LOGGER.error("写入数据被中断，" + event, e);
+        }
+    }
+
+    /**
+     * 处理GridFS
+     *
+     * @param event 事件
+     */
+    private void handleGridFS(Map event) {
+        String columnName;
+        String fileName;
+        BsonObjectId bsonObjectId = null;
+        GridFSUploadStream uploadStream = null;
+
+        for (Map.Entry<String, String> columnEntry : gridfsColumns.entrySet()) {
+            columnName = columnEntry.getKey();
+            fileName = Formatter.format(event, columnEntry.getValue());
+
+            byte[] data = (byte[]) event.get(columnName);
+            if (data == null || data.length <= 0) {
+                continue;
+            }
+
+            try {
+                bsonObjectId = new BsonObjectId();
+                uploadStream = gridFSBucket.openUploadStream(bsonObjectId, fileName, options);
+                uploadStream.write(data);
+            } catch (Exception e) {
+                LOGGER.error("GridFS上传失败，文件名：" + fileName, e);
+            } finally {
+                if (uploadStream != null) {
+                    uploadStream.close();
+                }
+                if (bsonObjectId != null) {
+                    event.put(columnName, bsonObjectId.getValue().toHexString());
+                }
+            }
         }
     }
 
     @Override
     public void release() {
-        bulkThread.stop();
+        receiverThreadExecutor.shutdownNow();
+
         try {
-            future.get();
+            bulkThreadExecutor.awaitTermination(5, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
-            logger.error("interrupted", e);
-        } catch (ExecutionException e) {
-            logger.error("execute error", e);
+            LOGGER.error("BulkThread被中断", e);
+        } finally {
+            bulkThreadExecutor.shutdown();
         }
-        executor.shutdown();
 
         if (client != null) {
             client.close();
@@ -126,58 +187,57 @@ public class MongoDB extends BaseOutput {
 
     private void submit(List<WriteModel<Document>> list) {
         if (list.size() > 0) {
-            BulkWriteResult writeResult = null;
             try {
-                writeResult = coll.bulkWrite(list, new BulkWriteOptions().ordered(false));
+                coll.bulkWrite(list, new BulkWriteOptions().ordered(false));
             } catch (Exception e) {
-                logger.error("bulk write data error!", e);
+                LOGGER.error("bulk write data error!", e);
             }
         }
     }
 
-    private class BulkThread implements Runnable {
+    private class ReceiverThread implements Runnable {
 
-        private Long submitTimestamp = System.currentTimeMillis();
-
-        private volatile boolean stop;
-
-        private List<WriteModel<Document>> cachedModelList = new ArrayList<WriteModel<Document>>(size);
-
-        private long position;
+        private List<WriteModel<Document>> cachedModelList = new ArrayList<WriteModel<Document>>(size + 1);
 
         public void run() {
-            while (!stop) {
-                WriteModel<Document> writeModel = writeModelQueue.poll();
-                if (writeModel != null) {
+            WriteModel<Document> writeModel;
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    writeModel = writeModelQueue.take();
                     cachedModelList.add(writeModel);
-                    position++;
 
-                    if (position % size == 0 || System.currentTimeMillis() - submitTimestamp >= flush_interval) {
-                        flush();
-                    }
-                } else {
-                    try {
-                        flush();
-                        Thread.sleep(flush_interval);
-                    } catch (InterruptedException e) {
-                        logger.error("interrupted", e);
+                    if (cachedModelList.size() >= size) {
+                        // 提交一批数据
+                        Future<?> future = bulkThreadExecutor.submit(new BulkThread(cachedModelList));
+                        cachedModelList = new ArrayList<WriteModel<Document>>(size + 1);
                     }
                 }
+            } catch (InterruptedException e) {
+                LOGGER.error("MongoDB的ReceiverThread被中断", e);
+            } finally {
+                flush();
             }
-
-            flush();
         }
 
-        public void flush() {
+        private void flush() {
             if (cachedModelList.size() > 0) {
                 submit(cachedModelList);
-                cachedModelList.clear();
-                submitTimestamp = System.currentTimeMillis();
             }
         }
 
-        public void stop() {
-            stop = true;
+    }
+
+    private class BulkThread implements Runnable {
+
+        private List<WriteModel<Document>> cachedModelList;
+
+        public BulkThread(List<WriteModel<Document>> cachedModelList) {
+            this.cachedModelList = cachedModelList;
         }
+
+        public void run() {
+            submit(cachedModelList);
+        }
+
     }
 }
